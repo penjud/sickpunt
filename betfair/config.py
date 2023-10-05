3ffeb77c2394560c129dcec6df5e7c3ac229c69b
Nicolas Dickreuter <dickreuter@gmail.com>
2023-10-05 13:01:28 +0100

    add uk

--- betfair/config.py
+++ betfair/config.py
@@ -31,7 +31,7 @@ orders_collection = mongo_db["orders"]
 strategy_collection = mongo_db["strategies"]
 
 # constants
-COUNTRIES = ['AU']
+COUNTRIES = ['AU', 'UK']
 MARKET_TYPES = ['WIN']
 EVENT_TYPE_IDS = ['7']  # Horse Racing event type ID
 
